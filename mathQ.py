896687cbf18814d84f5caa4a7d507ca4f94607e5
Valentin <soarevalentinn@gmail.com>
2021-12-19 16:07:55 +0200

    adding final mathQ.py Math Exercises Computer-Assisted Instruction

--- mathQ.py
+++ mathQ.py
@@ -15,9 +15,11 @@ def lines(number_of_lines):
 
 
 def print_header():
-    lines(60)
+    #lines(60)
     print(f"\033[1m {'MATH EXERCISES':>35} \033[0m", end="\n")
-    lines(60)
+    print(f"\033[1m {'*add, subtract, multiplication and divide*':>49} \033[0m", end="\n")
+    print(f"\033[1m {'[cht] (change operations), [chd] (change difficulty)':>54} \033[0m", end="\n")
+    #lines(60)
 
 
 def print_difficulty_levels():
@@ -46,7 +48,7 @@ def print_difficulty_levels():
             time.sleep(0.5)
             sys.exit(0)
         elif req_answer == 'cht':
-            value_if_right = 1
+            value_if_right = 2
         else:
             print(f"\n\033[1;31m Wrong answer !!!\033[0m", end="\n\n")
             time.sleep(0.5)
@@ -73,7 +75,7 @@ def execute_difficulty():
     answer_from_difficulty = 0
     value_to_check = 0
 
-    while value_to_check != 1:
+    while value_to_check != 1 and value_to_check != 2:
         os.system('clear')
         print_header()
         answer_from_difficulty, value_to_check = print_difficulty_levels()
@@ -81,8 +83,21 @@ def execute_difficulty():
     return answer_from_difficulty
 
 
-#def execute_operations_and_validate(numbering1, numbering2, type):
-#    operations = [(numbering1 + numbering2), (numbering1 - numbering2), (numbering1 * numbering2), (numbering1 / numbering2)]
+def execute_operations_and_validate(numbering1, numbering2, answer):
+    operations = [(numbering1 + numbering2), (numbering1 - numbering2), (numbering1 * numbering2), ('%.1f' % (numbering1 / numbering2))]
+    var_to_exit = 1
+
+    for i in range(len(operations)):
+        if (i == 3 and str(operations[i]).rstrip('.0') == answer) or (str(operations[i]) == answer):
+            print(f"\033\n[1;32m Right answer, keep it going !!\033[0m", end="\n")
+            var_to_exit = 0
+            time.sleep(0.5)
+            return var_to_exit
+
+    print(f"\n\033[1;31m Wrong answer !!!\033[0m", end="\n\n")
+    time.sleep(0.5)
+
+    return var_to_exit
 
 
 def ask_questions(nrm1, nrm2, oper_type):
@@ -106,7 +121,9 @@ def ask_questions(nrm1, nrm2, oper_type):
                 time.sleep(0.5)
                 sys.exit(0)
             elif swap_answer == 'chd':
-                operation_to_exit = 1
+                operation_to_exit = 3
+            elif swap_answer == 'cht':
+                operation_to_exit = 2
             else:
                 print(f"\n\033[1;31m Wrong answer !!!\033[0m", end="\n\n")
                 time.sleep(0.5)
@@ -119,7 +136,7 @@ def execute_questions(numbering1, numbering2, type_of_o):
     value = 0
     answr = 0
 
-    while value != 1:
+    while value != 1 and value != 3 and value != 2:
         os.system('clear')
         print_header()
         answr, value = ask_questions(numbering1, numbering2, type_of_o)
@@ -127,22 +144,84 @@ def execute_questions(numbering1, numbering2, type_of_o):
     return answr
 
 
-def main():
-    count_answers = 0
-    return_difficulty = execute_difficulty()
+def print_operations():
+    given_operations = [(1, "Add"), (2, "Subtract"), (3, "Multiply"), (4, "Divide")]
+    value_if_ok = 0
 
-    while count_answers <= 20:
-        nr1, nr2 = generate_numbers(return_difficulty)
-        answering = execute_questions(nr1, nr2, 2)
-        equation = nr1 - nr2
+    print(f"\n - > Select Operations: ", end="\n\n")
 
-        if str(equation) == answering:
-            print("\n Right answer !!", end="\n")
-            time.sleep(0.5)
-            count_answers += 1
+    for i in given_operations:
+        print(f"  {i[0]} - {i[1]}")
+
+    print(f"\n\n - > Answer (q, chd): ", end="")
+    answer = input()
+
+    try:
+        answer = int(answer)
+
+        if 1 <= answer <= 4:
+            value_if_ok = 1
+            return answer, value_if_ok
         else:
-            print(f"\n Wrong answer!!", end="\n")
+            print(f"\n \033[1;31m Wrong answer !!!\033[0m", end="\n\n")
             time.sleep(0.5)
+    except ValueError:
+        answer = answer.lower()
+
+        if answer == "q":
+            print(f"\n\033[1;36m :( Quitting...\033[0m", end="\n\n")
+            time.sleep(0.5)
+            sys.exit(0)
+        elif answer == "chd":
+            value_if_ok = 3
+        else:
+            print(f"\n \033[1;31m Wrong answer !!!\033[0m", end="\n\n")
+            time.sleep(0.5)
+
+    return answer, value_if_ok
+
+
+def execute_print_operations():
+    value = 0
+    answering = 0
+
+    while value != 1 and value != 3:
+        os.system('clear')
+        print_header()
+        answering, value = print_operations()
+
+    return answering
+
+
+def main():
+    val_to_exit = 0
+    return_difficulty = ''
+    type_of_o = ''
+    nr1 = 0
+    nr2 = 0
+
+    while True:
+        if not isinstance(type_of_o, int):
+            type_of_o = execute_print_operations()
+            swap_type_o = type_of_o
+
+        if not isinstance(return_difficulty, int):
+            return_difficulty = execute_difficulty()
+            if return_difficulty == 'cht':
+                type_of_o = ''
+                continue
+
+        if val_to_exit == 0:
+            nr1, nr2 = generate_numbers(return_difficulty)
+
+        answering = execute_questions(nr1, nr2, swap_type_o)
+
+        if answering == 'chd':
+            return_difficulty = ''
+        elif answering == 'cht':
+            type_of_o = ''
+        else:
+            val_to_exit = execute_operations_and_validate(nr1, nr2, answering)
 
 
 main()
