ef8982ed41885ad752db2c6a4c580b1f864f3f1e
Valentin <soarevalentinn@gmail.com>
2021-12-22 10:21:34 +0200

    small mod

--- mathQ.py
+++ mathQ.py
@@ -20,7 +20,7 @@ def lines(number_of_lines):
 def print_header():
     lines(70)
     print(f"\033[1m {'MATH EXERCISES':>40}\n {'*add, subtract, multiplication and divide*':>55}\n "
-          f"{'[cht] (change operations), [chd] (change difficulty), [q] (quit)':>66}  \033[0m", end="\n")
+          f"{'[cht] (change equations), [chd] (change difficulty), [q] (quit)':>66}  \033[0m", end="\n")
     lines(70)
 
 
