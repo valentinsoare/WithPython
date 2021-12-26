c72d3a24b35d1bc0eac413f16d094cf1be122f4f
Valentin <soarevalentinn@gmail.com>
2021-12-26 20:18:27 +0200

    way cool

--- stringPalindrome.py
+++ stringPalindrome.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python
 
-string = 'House'
+string = '14541'
 
 
 def is_palindrome(given_string):
     processed_list = list(given_string.lower())
     length_of_a_list = len(processed_list)
+    ranges = len(processed_list) // 2
     count = 0
+    i = 0
 
-    for i in range(length_of_a_list // 2):
+    while i < ranges:
         if processed_list.pop() == processed_list.pop(0):
             count += 1
+        i += 1
 
     if count == length_of_a_list // 2:
         print(f"\n\033[1;32m - > String is a palindrome.\033[0m", end="\n\n")
