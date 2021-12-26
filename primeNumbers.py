3bee988ead412a927a666bb668905c34e2d02fc4
Valentin <soarevalentinn@gmail.com>
2021-12-26 18:55:57 +0200

    update v2

--- primeNumbers.py
+++ primeNumbers.py
@@ -20,7 +20,7 @@ def check_if_prime(given_number):
 
 def extract_prime_numbers_from_list(given_list):
     prime_numbers = list(filter(check_if_prime, given_list))
-    print(f"\n - > Prime numbers from the given list: {prime_numbers}")
+    print(f"\n - > Prime numbers from the given list:\n\n {prime_numbers}", end="\n\n")
 
 
 def main():
