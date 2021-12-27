4f31016ae2890affb5d9a762c4bfb92c11cc0e47
Valentin <soarevalentinn@gmail.com>
2021-12-27 04:12:33 +0200

    update for stringPalindrome.py

--- stringPalindrome.py
+++ stringPalindrome.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python
 
-string = '14541'
+import string
+
+punctuation_list = string.punctuation
+our_string = 'lux,Si,       opulenta@$&'
 
 
 def is_palindrome(given_string):
-    processed_list = list(given_string.lower())
+    processed_list = list(filter(lambda k: k not in punctuation_list and k != ' ' and k != string.whitespace, list(given_string.lower())))
+    print(f"{processed_list}")
     length_of_a_list = len(processed_list)
     ranges = len(processed_list) // 2
     count = 0
@@ -15,7 +19,7 @@ def is_palindrome(given_string):
             count += 1
         i += 1
 
-    print(f'\n\033[1m String to check: "{string}"\033[0m', end="\n")
+    print(f'\n\033[1m String to check: "{given_string}"\033[0m', end="\n")
 
     if count == length_of_a_list // 2:
         print(f"\n\033[1;32m - > String is a palindrome.\033[0m", end="\n\n")
@@ -24,7 +28,7 @@ def is_palindrome(given_string):
 
 
 def main():
-    is_palindrome(string)
+    is_palindrome(our_string)
 
 
 main()
