a322dcbda0c1aa3afd4af5ed79b3c00a882c2de9
Valentin <soarevalentinn@gmail.com>
2022-01-02 10:09:27 +0200

    some new modifications

--- representingInvoices.py
+++ representingInvoices.py
@@ -17,17 +17,17 @@ def generate_sample_hw_data(*args):
 def sort_given_list_tuples(given_list_of_tuples, element_of_sort):
     if element_of_sort == 1:
         sorted_by_description = sorted(given_list_of_tuples, key=operator.itemgetter(1))
-        print(f"{sorted_by_description}")
+        print(f"\n\033[1m - > Sorted invoices by description:\033[0m \n{sorted_by_description}", end="\n")
     elif element_of_sort == 3:
         sorted_by_price = sorted(given_list_of_tuples, key=operator.itemgetter(3), reverse=True)
-        print(f"{sorted_by_price}")
+        print(f"\n\033[1m - > Sorted invoices by price: \033[0m \n{sorted_by_price}", end="\n")
 
 
 def mapping_to_description_and_quantity(given_list_invoices):
     mapping_to = operator.itemgetter(1, 2)
 
     list_of_description_qty = sorted(list(map(mapping_to, given_list_invoices)), key=operator.itemgetter(1))
-    print(f"{list_of_description_qty}")
+    print(f"\n\033[1m - > List from description and quantity and then sort by quantity:\033[0m\n {list_of_description_qty}", end="\n")
 
 
 def mapping_to_description_invoice_value(given_list_invoices, starting=0, stopping=0):
@@ -46,8 +46,6 @@ def mapping_to_description_invoice_value(given_list_invoices, starting=0, stoppi
                 final_after_product += [(i, j * k), ]
 
     sorted_by_invoice_value = sorted(final_after_product, key=operator.itemgetter(1), reverse=True)
-
-    #print(f"{sorted_by_invoice_value}")
 
     return sorted_by_invoice_value
 
@@ -70,20 +68,25 @@ def main():
 
     hw_data = generate_sample_hw_data(part_number, part_description, quantity, price)
 
-    # sort by description
+    print(f"\n\033[1m**Pattern in the tuples from the list of tuples: part_number, part_description, quantity, price\033[0m"
+          , end="\n")
+
+    #sort by description
     sort_given_list_tuples(hw_data, 1)
 
-    ## sort by price
+    #sort by price
     sort_given_list_tuples(hw_data, 3)
 
-    ## description and quantity and sort by qty
+    #description and quantity and sort by qty
     mapping_to_description_and_quantity(hw_data)
 
-    ## mapping description and invoice values and sort by invoice value
-    mapping_to_description_invoice_value(hw_data)
+    #mapping description and invoice values and sort by invoice value
+    sort_by_all_invoice_values = mapping_to_description_invoice_value(hw_data, 0, 0)
+    print(f"\n\033[1m - > Description and invoice values and sort by invoice in descending:\033[0m\n {sort_by_all_invoice_values}", end="\n")
 
-    ## description and invoice value but invoice value in a given range
-    mapping_to_description_invoice_value(hw_data, 0, 1000)
+    #description and invoice value but invoice value in a given range
+    given_range_to_print = mapping_to_description_invoice_value(hw_data, 0, 1000)
+    print(f"\n\033[1m - > Description and invoice in descending order in a given range:\033[0m\n {given_range_to_print}", end="\n")
 
     given_data_invoices = mapping_to_description_invoice_value(hw_data, 0, 0)
     calculate_total_invoices(given_data_invoices)
