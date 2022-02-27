520aa6ea44aa9d0786cc307990cb2d54fd1ab3f6
Valentin <soarevalentinn@gmail.com>
2022-02-27 02:57:54 +0200

    metricConversion for length completed

--- metricConversion.py
+++ metricConversion.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 
-"""We have system to system conversion (metric <-> U.S. System)"""
-
 
 def catch_given_input():
     print(f'\n\033[1m**Enter your desired conversion...\033[0m\n')
@@ -36,34 +34,30 @@ def determine_from_what_to_what(from_type, *arguments):
     return x, y
 
 
-def conversion_english_to_meters(*arguments):
+def from_system_to_meters(*arguments):
     many_qty, type_of_value, dict_of_values = arguments
     var_to_return = 0
 
     dict_to_read = dict_of_values.items()
     for i, j in dict_to_read:
-        if i == type_of_value:
+        if i == type_of_value.lower():
             var_to_return = many_qty * j
 
     return var_to_return
 
 
-def from_meters_to_metric(value_in_meters, to_type, dict_of_values_metric_system_length):
+def from_meters_to_system(value_in_meters, to_type, dict_of_values_metric_system_length, type_of_oper = 0):
     value_after_conv = 0
 
     dict_to_read = dict_of_values_metric_system_length.items()
     for i, j in dict_to_read:
         if i == to_type.lower():
-            value_after_conv = value_in_meters * j
+            if type_of_oper == 1:
+                value_after_conv = value_in_meters * j
+            else:
+                value_after_conv= value_in_meters / j
 
     return value_after_conv
-
-
-def from_metric_to_meters(*arguments):
-    value_in_meters = 0
-    many_qty, type_of_value, dict_of_values_metric_system_length = arguments
-
-    return value_in_meters
 
 
 def main():
@@ -77,17 +71,18 @@ def main():
 
     from_value, to_value = determine_from_what_to_what(type_of_value, dict_of_values_english_system_length,
                                                        dict_of_values_metric_system_length)
-    print(f'\n{"-"*50}')
+    print(f'\n{"-"*45}')
     print(f'\033[1m**Conversion: {many_qty} {type_of_value} =', end=" ")
 
     if from_value == 1:
-        from_english_to_meters = conversion_english_to_meters(many_qty, type_of_value, dict_of_values_english_system_length)
-        metric_after_conv = from_meters_to_metric(from_english_to_meters, to_measurements, dict_of_values_metric_system_length)
+        from_english_to_meters = from_system_to_meters(many_qty, type_of_value, dict_of_values_english_system_length)
+        metric_after_conv = from_meters_to_system(from_english_to_meters, to_measurements, dict_of_values_metric_system_length, 1)
         print(f'{metric_after_conv:.2f} {to_measurements}\033[0m', end="\n")
     elif from_value == 2:
-        metric_to_meters = from_metric_to_meters(many_qty, type_of_value, dict_of_values_metric_system_length)
-
-    print(f'{"-" * 50}')
+        metric_to_meters = from_system_to_meters(many_qty, type_of_value, dict_of_values_metric_system_length)
+        meters_to_english = from_meters_to_system(metric_to_meters, to_measurements, dict_of_values_english_system_length, 0)
+        print(f'{meters_to_english:.2f} {to_measurements}\033[0m', end="\n")
+    print(f'{"-" * 45}', end="\n\n")
 
 
 main()
