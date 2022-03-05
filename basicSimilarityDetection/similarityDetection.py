f957d6dc0e4c6ba15ade40c00d01ec9bacac2ef8
Valentin <soarevalentinn@gmail.com>
2022-03-05 22:13:53 +0200

    similarity Detection

---
+++ basicSimilarityDetection/similarityDetection.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+import re
+from os import system
+from time import sleep
+from statistics import mean
+
+
+def catch_number_of_texts():
+    print(f'\n{" - > How many texts you want to compare:"}', end=" ")
+    number_of_texts = input()
+
+
+
+def main():
+    print(f'XXXXX')
+
+
+main()
+
