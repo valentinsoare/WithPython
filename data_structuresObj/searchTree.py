3a43d081d7a21a8ceafaad1ccd0573b9d7ca4bca
Valentin <soarevalentinn@gmail.com>
2022-06-11 00:51:34 +0300

    BST under learning :D

--- data_structuresObj/searchTree.py
+++ data_structuresObj/searchTree.py
@@ -2,3 +2,85 @@
 
 from stackWithArrays import StacksArray
 from queueArray import QueueArray
+
+
+class Node:
+    __slots__ = '_element', '_left', '_right'
+
+    def __init__(self, element, left, right):
+        self._element = element
+        self._left = left
+        self._right = right
+
+    @property
+    def element(self):
+        return self._element
+
+    @property
+    def left(self):
+        return self._left
+
+    @property
+    def right(self):
+        return self._right
+
+
+class BinarySearchTree:
+    def __init__(self, element, left, right):
+        if None in (left, right):
+            self._root = Node(element, None, None)
+        else:
+            self._root = Node(element, left.root, right.root)
+
+    @property
+    def root(self):
+        return self._root
+
+    def iterative_search(self, given_value):
+        tnode = self.root
+
+        while tnode.element is not None:
+            if given_value is tnode.element:
+                return True
+            elif given_value < tnode.element:
+                tnode = tnode.left
+            elif given_value > tnode.element:
+                tnode = tnode.right
+
+        return False
+
+    def recursive_search(self, given_value, given_node):
+        temp_root = given_node.element
+
+        if temp_root is None:
+            return False
+
+        if given_value == temp_root:
+            return True
+        elif given_value > temp_root:
+            return self.recursive_search(given_value, given_node.right)
+        else:
+            return self.recursive_search(given_value, given_node.left)
+
+    def iterative_traverse_binary_search_tree(self):
+        temp_root = self.root
+        given_stack = StacksArray()
+
+        while True:
+            if temp_root.element is not None:
+                given_stack.push(temp_root)
+                temp_root = temp_root.left
+            elif not given_stack.is_empty():
+                temp_root = given_stack.pop()
+                yield temp_root.element
+                temp_root = temp_root.right
+            else:
+                break
+
+    def recursive_traverse_binary_search_tree(self, given_node, given_list):
+        if given_node.element:
+            self.recursive_traverse_binary_search_tree(given_node.left, given_list)
+            given_list.append(given_node.element)
+            self.recursive_traverse_binary_search_tree(given_node.right, given_list)
+
+        return given_list
--- data_structuresObj/second_run.py
+++ data_structuresObj/second_run.py
@@ -1,46 +1,72 @@
 #!/usr/bin/python
 
-from binTree import BinaryTree
+#from binTree import BinaryTree
+from searchTree import BinarySearchTree
 
 
 def main():
-    null_node = BinaryTree()
+    #null_node = BinaryTree()
 
-    a = BinaryTree()
-    b = BinaryTree()
-    c = BinaryTree()
-    d = BinaryTree()
-    e = BinaryTree()
-    f = BinaryTree()
-    g = BinaryTree()
-    h = BinaryTree()
-    i = BinaryTree()
-    j = BinaryTree()
-    k = BinaryTree()
-    m = BinaryTree()
+    #a = BinaryTree()
+    #b = BinaryTree()
+    #c = BinaryTree()
+    #d = BinaryTree()
+    #e = BinaryTree()
+    #f = BinaryTree()
+    #g = BinaryTree()
+    #h = BinaryTree()
+    #i = BinaryTree()
+    #j = BinaryTree()
+    #k = BinaryTree()
+    #m = BinaryTree()
 
-    n = BinaryTree()
+    #n = BinaryTree()
 
-    a.make_tree(33, null_node, null_node)
-    b.make_tree(39, null_node, null_node)
-    c.make_tree(37, a, b)
-    d.make_tree(25, null_node, null_node)
-    e.make_tree(65, null_node, null_node)
-    f.make_tree(72, null_node, null_node)
-    g.make_tree(31, d, c)
-    h.make_tree(68, e, f)
-    i.make_tree(49, null_node, null_node)
-    j.make_tree(55, null_node, null_node)
-    k.make_tree(40, g, i)
-    m.make_tree(60, j, h)
+    #a.make_tree(33, null_node, null_node)
+    #b.make_tree(39, null_node, null_node)
+    #c.make_tree(37, a, b)
+    #d.make_tree(25, null_node, null_node)
+    #e.make_tree(65, null_node, null_node)
+    #f.make_tree(72, null_node, null_node)
+    #g.make_tree(31, d, c)
+    #h.make_tree(68, e, f)
+    #i.make_tree(49, null_node, null_node)
+    #j.make_tree(55, null_node, null_node)
+    #k.make_tree(40, g, i)
+    #m.make_tree(60, j, h)
 
-    n.make_tree(50, k, m)
+    #n.make_tree(50, k, m)
 
-    print(n)
-    ax = k.successor(k.root)
-    print(ax)
+    #print(n)
+    #ax = k.successor(k.root)
+    #print(ax)
 
     #---------------------------------------
+
+    zero_node = BinarySearchTree(None, None, None)
+
+    a = BinarySearchTree(1, zero_node, zero_node)
+    b = BinarySearchTree(4, zero_node, zero_node)
+    c = BinarySearchTree(3, a, b)  # subtree root
+
+    d = BinarySearchTree(6, zero_node, zero_node)
+    e = BinarySearchTree(9, zero_node, zero_node)
+    f = BinarySearchTree(8, d, e)  # sbutree root
+
+    g = BinarySearchTree(5, c, f)
+
+    #if g.iterative_search(3):
+    #    print(f'nebunie boss')
+
+    #ax = g.recursive_search(, g.root)
+    #print(ax)
+
+    #ax = list(f.iterative_traverse_binary_search_tree())
+    #print(ax)
+
+    ay = list(g.recursive_traverse_binary_search_tree(g.root, []))
+    print(ay)
+
 
 
 if __name__ == "__main__":
