cba6f5db6ba81e62256d8ddb75e8b37a2fb9a292
Valentin <soarevalentinn@gmail.com>
2022-10-25 10:34:29 +0300

    more scripts added, vanityPhoneNumbers

---
+++ relearningPy/vanityPhoneNumbers.py
@@ -0,0 +1,109 @@
+#!/usr/bin/python
+
+import os
+import time
+import string
+import itertools
+
+
+def prepare_message_for_header(given_message):
+    word = ''
+    list_with_header = []
+    message_to_display = ''
+
+    for i in given_message:
+        if i == ' ':
+            list_with_header.append(word)
+            word = ''
+        else:
+            word += i
+
+    list_with_header.append(word)
+
+    for i in list_with_header:
+        message_to_display += ' ' + '*' + ' ' + i
+
+    message_to_display += ' ' + '*'
+    return message_to_display
+
+
+def printing_header(given_message):
+    length_of_message = len(given_message)
+    print(f"\n\n{' ' * 12}{' ' * (length_of_message // 2 - 1)}{given_message}")
+    print(f"{' ' * 12}{'-' * (length_of_message * 2)}")
+
+
+def map_letters_digit():
+    count = 2
+    left = 0
+    right = 3
+    letters_from_alphabet = list(string.ascii_lowercase)
+    letters_mapped = []
+
+    while count < 10:
+        if count == 7 or count == 9:
+            right += 1
+            letters_mapped.append([count, (letters_from_alphabet[left:right])])
+            left += 1
+        else:
+            letters_mapped.append([count, tuple(letters_from_alphabet[left:right])])
+
+        left += 3
+        right += 3
+        count += 1
+
+    return letters_mapped
+
+
+def ask_a_question():
+    error = 1
+    processing_answer = ''
+    message_to_print = prepare_message_for_header('Vanity Phone Numbers')
+
+    while error == 1:
+        os.system('clear')
+        printing_header(message_to_print)
+
+        print(f"{' ' * 11} * You want to give a string to convert to digits (q to quit):", end=" ")
+        answer = input()
+
+        if answer.lower()[0] == 'q':
+            print(f"\n{' ' * 14} Exiting...")
+            os.system('sleep 1')
+            exit(1)
+
+        try:
+            processing_answer = int(answer)
+        except ValueError:
+            error = 0
+            processing_answer = answer.lower()
+
+        if error == 1:
+            print(f"\n{' ' * 5} \033[1;31m ERROR - Please do not use integers/floats. We need alpha characters. \033[0m")
+            os.system('sleep 2')
+
+    return processing_answer
+
+
+def from_string_to_digit(list_mappings, given_string):
+    number_after_conversion = ''.join(list(str(j[0]) for i in given_string for j in list_mappings if i in j[1]))
+
+    return number_after_conversion
+
+
+def print_the_result(given_string, converted_string):
+    os.system('sleep 0.5')
+    print(f"\n{' ' * 11} * \033[1;32mSUCCESS\033[0m From \"{given_string}\" converted to \"{converted_string}\"\n")
+    exit(1)
+
+
+def main():
+    list_with_mappings = map_letters_digit()
+    value_from_the_user = ask_a_question()
+
+    value_after_conversion = from_string_to_digit(list_with_mappings, value_from_the_user)
+    print_the_result(value_from_the_user, value_after_conversion)
+
+
+if __name__ == '__main__':
+    main()
--- relearningPy/wordGeneratorByPhone.py
+++ relearningPy/wordGeneratorByPhone.py
@@ -1,6 +1,42 @@
 #!/usr/bin/python
 
+import os
 import string
+import itertools
+
+
+def printing_header(message):
+    length_message = len(message)
+    print(f"\n{' ' * 12}{' ' * (length_message // 2)}{message}")
+    print(f"{' ' * 12}{'-' * (length_message * 2)}")
+
+
+def ask_a_question():
+    processed_answer = ''
+    error = 0
+    answer = ''
+
+    while not isinstance(processed_answer, int):
+        os.system('clear')
+        printing_header('Letters from phone number')
+        print(f"{' ' * 11} - > Please give us the phone number with 7 digits or more:", end=" ")
+        answer = input()
+
+        try:
+            processed_answer = int(answer)
+        except ValueError:
+            error = 1
+
+        if error == 1 or processed_answer < 0 or len(str(processed_answer)) < 7:
+            print(f"\n{' ' * 7} \033[1;31m ERROR \033[0m - please give us another number, positive with 7 digits or more.")
+            os.system('sleep 2')
+            error = 0
+
+    if answer[0] == '0':
+        to_return = answer[0] + answer[1:len(answer)]
+        return to_return
+    else:
+        return str(processed_answer)
 
 
 def map_digits_letters():
@@ -8,10 +44,16 @@ def map_digits_letters():
     right = 3
     count = 2
     list_with_mapping = []
-    given_letters_list = list(letter for letter in string.ascii_lowercase if letter not in ['q', 'z'])
+    given_letters_list = list(string.ascii_letters)
 
     while count != 10:
-        list_with_mapping.append([count, tuple(given_letters_list[left:right])])
+        if count == 7 or count == 9:
+            right += 1
+            list_with_mapping.append([count, tuple(given_letters_list[left:right])])
+            left += 1
+        else:
+            list_with_mapping.append([count, tuple(given_letters_list[left:right])])
+
         left += 3
         right += 3
         count += 1
@@ -19,8 +61,45 @@ def map_digits_letters():
     return list_with_mapping
 
 
+def extract_letters_by_digits(given_list_letters_digits, phone_number):
+    list_to_work_with = []
+
+    for i in str(phone_number):
+        if i in ['0', '1']:
+            list_to_work_with.append(i)
+        for j in given_list_letters_digits:
+            if i == str(j[0]):
+                list_to_work_with.append(j[1])
+
+    return list_to_work_with
+
+
+def combine_letters(given_list_with_letters):
+    list_from_product = list(''.join(i) for i in itertools.product(*given_list_with_letters))
+    return list_from_product
+
+
+def printing_result(list_with_combinations):
+    length_of_an_element = len(list_with_combinations[0])
+
+    print(f"\n - >  All possible combinations of letters:")
+
+    for i in range(len(list_with_combinations)):
+        if i % 10 == 0:
+            print(f"\n{' ' * length_of_an_element}", end="")
+        else:
+            print(f"{list_with_combinations[i]}", end=" ")
+
+
 def main():
     mapping_digits_letters = map_digits_letters()
+    number_to_work_on = ask_a_question()
+
+    letters_to_work_with = extract_letters_by_digits(mapping_digits_letters, number_to_work_on)
+
+    all_possible_combinations = combine_letters(letters_to_work_with)
+
+    printing_result(all_possible_combinations)
 
 
 if __name__ == '__main__':
