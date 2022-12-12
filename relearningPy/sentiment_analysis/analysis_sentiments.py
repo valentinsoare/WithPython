ff4f266bbe3d1e3a5c081eb797122f5ff979f83b
Valentin <soarevalentinn@gmail.com>
2022-12-12 14:22:59 +0200

    all done with sentiment analysis, but if I have the time for it one more thing to add.

--- relearningPy/sentiment_analysis/analysis_sentiments.py
+++ relearningPy/sentiment_analysis/analysis_sentiments.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python
 
-import os
+
+"""this is a cool way, but rough around the edges, to do sentiment analysis without any ddicated library. i used only a list with text which contains#
+negative words and positive and each words has a numeric value, positive or negative associated with it.
+made by Valentin S."""
+
+
 import re
 import logging
 import pandas as pd
 from time import sleep
+from os import system, path
 from random import randrange
 from collections import Counter
 from pyparsing import Word, alphas
@@ -139,11 +145,11 @@ def validate_load_txt_file(given_answer):
     given_message_1 = f"{' ' * 4}{' ERROR the file does not exists.'}"
     given_message_2 = f"{' ' * 4}{' ERROR the file is empty.'}"
 
-    if not os.path.isfile(given_answer):
+    if not path.isfile(given_answer):
         print(f"\n{given_message_1}\n")
         sleep(sleep_time)
     else:
-        if os.path.getsize(given_answer) == 0:
+        if path.getsize(given_answer) == 0:
             print(f"{' ' * 4}{given_message_2}\n")
             sleep(sleep_time)
         else:
@@ -157,8 +163,8 @@ def read_input_from_command_line():
     answer = ''
 
     while to_exit == 0:
-        os.system('clear')
-        #header_printing(header_name)
+        system('clear')
+        header_printing(header_name)
 
         print(f"{' ' * 2} * Please put here the sentence(s) you want to analyze (q to quit or b to return to "
               f"sources:", end=" ")
@@ -174,8 +180,8 @@ def load_input_from_text_file():
     answer = ''
 
     while error == 0:
-        os.system('clear')
-        #header_printing(header_name)
+        system('clear')
+        header_printing(header_name)
         print(f"{' ' * 2} * Please enter the entire path of the file which is use "
               f"to load the text from (q to quit or b to return to sources):", end=" ")
         answer = input()
@@ -196,8 +202,8 @@ def ask_for_input_source():
     dict_with_options = {'1.': 'Command Line', '2.': 'Text File'}
 
     while error == 0:
-        os.system('clear')
-        #header_printing(header_name)
+        system('clear')
+        header_printing(header_name)
         print(f"{' ' * 2}{' * From where you want to load the text to analyse, command line or file:'}\n")
 
         for i, j in dict_with_options.items():
@@ -213,10 +219,8 @@ def ask_for_input_source():
 
 def prepare_text_for_analysis(given_text):
     punctuation = '!"#$%&\'()*+,./:;<=>?[\]`{|}~'
-    split_text = given_text.split()
-
-    split_without_punctuation_beginning_end = list(word.translate(str.maketrans('', '', punctuation)).lower()
-                                                   for word in split_text)
+    split_without_punctuation_beginning_end = pd.Series(word.translate(str.maketrans('', '', punctuation)).lower()
+                                                        for word in given_text.split())
     return split_without_punctuation_beginning_end
 
 
@@ -232,10 +236,11 @@ def creating_text_parser_for_level(given_level_of_log):
 
 
 def logging_the_output(logging_level_to_use, list_with_words):
-    # tested for security gaps and all is ok with using eval in this instance.
+    # tested for security gaps and all is ok with using eval in this case.
+    path_of_the_log_file = 'sentiments_values_analysis.log'
     given_level = creating_text_parser_for_level(logging_level_to_use)
 
-    logging.basicConfig(filename='sentiments_values_analysis.log',
+    logging.basicConfig(filename=path_of_the_log_file,
                         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                         datefmt='%d/%m/%Y %I:%M:%S %p',
                         level=eval(given_level))
@@ -245,22 +250,80 @@ def logging_the_output(logging_level_to_use, list_with_words):
 
 
 def populate_sentiment_value_dict(given_list_with_words, positive_words, negative_words):
-    positive_dict_with_words_from_text = {}
-    negative_dict_with_words_from_text = {}
-    positive_words_freq = {}
-    negative_words_freq = {}
+    global positive_dict_with_words_from_text
+    positive_dict_with_words_from_text = pd.Series(dtype=str)
+    global negative_dict_with_words_from_text
+    negative_dict_with_words_from_text = pd.Series(dtype=str)
+    global positive_words_freq
+    positive_words_freq = pd.Series(dtype=str)
+    global negative_words_freq
+    negative_words_freq = pd.Series(dtype=str)
 
-    counting_words_freq = Counter(given_list_with_words)
+    counting_words_freq = pd.Series(Counter(given_list_with_words))
 
     for word, freq in counting_words_freq.items():
         if word in positive_words.keys():
-            positive_dict_with_words_from_text[word] = positive_words.loc[word]
+            positive_dict_with_words_from_text[word] = (freq * positive_words.loc[word])
             positive_words_freq[word] = freq
         elif word in negative_words.keys():
-            negative_dict_with_words_from_text[word] = negative_words.loc[word]
+            negative_dict_with_words_from_text[word] = (freq * negative_words.loc[word])
             negative_words_freq[word] = freq
 
-    return counting_words_freq, positive_dict_with_words_from_text, positive_words_freq, negative_dict_with_words_from_text, negative_words_freq
+    return counting_words_freq, positive_dict_with_words_from_text, positive_words_freq, \
+        negative_dict_with_words_from_text, negative_words_freq
+
+
+def printing_statistics(total_count_freq, entire_text, positive_words, positive_freq, negative_words, negative_freq):
+    number_of_positive_words = positive_freq.values.sum()
+    five_most_used_positive_words = (positive_freq.sort_values(ascending=False)).iloc[0:6]
+    percent_of_appearances_of_positive = number_of_positive_words * 100 / len(entire_text)
+
+    number_of_negative_words = negative_freq.values.sum()
+    percent_of_appearances_of_negative = number_of_negative_words * 100 / len(entire_text)
+    five_most_used_negative_words = (negative_freq.sort_values(ascending=False)).iloc[0:6]
+
+    final_score = positive_words.values.sum() + negative_words.values.sum()
+    score_to_print_for_text = (final_score * 100) / len(entire_text)
+
+    system('clear')
+    header_printing(header_name)
+    if final_score < 0:
+        type_of_sentiments = f"negative"
+        score_to_print_for_text *= -1
+    elif final_score > 0:
+        type_of_sentiments = f"positive"
+    else:
+        type_of_sentiments = f"undetermined"
+
+    count = list(range(1, 5))
+    header_1 = f"{' ' * 4} {count[0]}. given text has a {type_of_sentiments.upper()} connotation: words with {type_of_sentiments} meaning are stronger by {score_to_print_for_text:.2f}%"
+
+    if type_of_sentiments == 'negative':
+        number_of_words = number_of_negative_words
+        percent_of_appearances = percent_of_appearances_of_negative
+        five_most_used = five_most_used_negative_words
+        header_1_continuation = f" compared to positive ones."
+    elif type_of_sentiments == 'positive':
+        number_of_words = number_of_positive_words
+        percent_of_appearances = percent_of_appearances_of_positive
+        five_most_used = five_most_used_positive_words
+        header_1_continuation = f" compared to negative ones."
+    else:
+        header_1 = f"\n{' ' * 8} :( Cannot determine which way the text is going due to the fact that the power of negative words is equal to that of positive ones.\n"
+        header_1_continuation = ""
+        exit(0)
+
+    content = f"\n{' ' * 4} {count[1]}. number of {type_of_sentiments} words: {number_of_words}"
+    content += f"\n{' ' * 4} {count[2]}. five most used {type_of_sentiments} words from the text:"
+    content += f"\n{' ' * 8}{'WORD':<15}{'FREQUENCY'}"
+
+    for i, j in five_most_used.items():
+        content += f"\n{' ' * 8}{i:<15}{j}"
+
+    content += f"\n{' ' * 4} {count[3]}. the percent of {type_of_sentiments} words: {percent_of_appearances:.2f}%"
+    final = header_1 + header_1_continuation + content
+
+    return final
 
 
 def main():
@@ -275,12 +338,17 @@ def main():
         if len(text_to_use) > 1:
             words_in_list_for_analysis = prepare_text_for_analysis(text_to_use)
             negative_sentiments, positive_sentiments = import_document_with_sentiments()
-            #logging_the_output('info', words_in_list_for_analysis)
 
             total_count_freq, positive_words, positive_freq, negative_words, negative_freq = populate_sentiment_value_dict(words_in_list_for_analysis,
                                                                                                                         positive_sentiments,
                                                                                                                         negative_sentiments)
-            
+            text_to_output = printing_statistics(total_count_freq, words_in_list_for_analysis, positive_words, positive_freq, negative_words, negative_freq)
+
+            print(text_to_output)
+            logging_the_output('info', f"\n" + text_to_output)
+
+            exit(0)
+
 
 if __name__ == '__main__':
     main()
